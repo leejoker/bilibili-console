b8a7b2ee8acb7e9504cd6c9ab75cc0fca7204e7b
leejoker <1056650571@qq.com>
2021-07-08 17:20:31 +0800

    add some initializations

--- lib/bilibili_console.rb
+++ lib/bilibili_console.rb
@@ -6,24 +6,27 @@ require_relative 'bilibili_console/fav'
 
 # bilibili console main class
 class BilibiliConsole
-  attr_accessor :http_client, :bilibili_login, :user
+  attr_accessor :http_client, :bilibili_login, :user, :fav
 
   def initialize
     @http_client = BiliHttp::HttpClient.new
+    @bilibili_login = Bilibili::Login.new(@http_client)
+    @fav = Bilibili::Fav.new(http_client)
   end
 
   def login
-    @bilibili_login = Bilibili::Login.new(http_client)
     @bilibili_login.login
   end
 
   def login_user_info
-    @user = @bilibili_login.login_user_info
-    @user.to_json
+    @bilibili_login.login_user_info
   end
 
   def user_fav_list
-    fav = Bilibili::Fav.new(http_client)
-    fav.list_user_fav_video(@user)
+    @fav.list_user_fav_video(@user)
+  end
+
+  def list_fav_video(media_id, page_num = 1, page_size = 10, keyword = nil)
+    @fav.list_fav_video(media_id, page_num, page_size, keyword)
   end
 end
