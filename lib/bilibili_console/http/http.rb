d415aac0224125f6953f2c2746b5f754839387dd
leejoker <1056650571@qq.com>
2021-07-22 11:31:58 +0800

    add manga.rb

--- lib/bilibili_console/http/http.rb
+++ lib/bilibili_console/http/http.rb
@@ -1,3 +1,8 @@
+# Copyright (c) 2021 leejoker
+#
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
 require 'net/http'
 require 'nice_http'
 require_relative 'response_body'
@@ -9,11 +14,12 @@ module BiliHttp
   end
   # bilibili http client
   class HttpClient
-    attr_accessor :login_http, :api_http
+    attr_accessor :login_http, :api_http, :manga_http
 
     def initialize
       @login_http = NiceHttp.new('http://passport.bilibili.com')
       @api_http = NiceHttp.new('http://api.bilibili.com')
+      @manga_http = NiceHttp.new('http://manga.bilibili.com')
       BiliHttp.headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Edg/91.0.864.64',
         'Referer': 'https://www.bilibili.com'
@@ -39,6 +45,17 @@ module BiliHttp
         headers: custom_headers,
         path: URI(url).request_uri,
         data: params
+      }
+      json_data(http.post(request).data)
+    end
+
+    def post_json(http, url, headers, req_body)
+      headers = BiliHttp.headers.clone if headers.nil? || headers.empty?
+      headers['Content-Type'] = 'application/x-www-form-urlencoded'
+      request = {
+        headers: headers,
+        path: URI(url).request_uri,
+        data: req_body
       }
       json_data(http.post(request).data)
     end
