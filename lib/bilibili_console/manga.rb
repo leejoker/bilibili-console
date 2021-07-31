bb8ad34319ab8af847fe545a8e84279c98a04dc2
leejoker <1056650571@qq.com>
2021-07-31 13:20:49 +0800

    change manga header

--- lib/bilibili_console/manga.rb
+++ lib/bilibili_console/manga.rb
@@ -12,11 +12,9 @@ module Bilibili
   # bilibili video interfaces
   class Manga < BilibiliBase
     def check_in
-      user_agent = <<~AGENT
-        Mozilla/5.0 BiliDroid/6.4.0 (bbcallen@gmail.com) os/android model/M1903F11I mobi_app/android build/6040500 channel/bili innerVer/6040500 osVer/9.0.0 network/2
-      AGENT
-      cur_header = {}
-      cur_header['User-Agent'] = user_agent
+      cur_header = {
+        'User-Agent': 'Mozilla/5.0 BiliDroid/6.4.0 (bbcallen@gmail.com) os/android model/M1903F11I mobi_app/android build/6040500 channel/bili innerVer/6040500 osVer/9.0.0 network/2'
+      }
       post_jsonm('https://manga.bilibili.com/twirp/activity.v1.Activity/ClockIn', cur_header,
                  { 'platform' => 'android' })
     end
