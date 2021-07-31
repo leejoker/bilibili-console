b8083effdb5bd031dc5be8953dcd591a167173f7
leejoker <1056650571@qq.com>
2021-07-31 11:48:33 +0800

    add manga checkin

--- lib/bilibili_console/manga.rb
+++ lib/bilibili_console/manga.rb
@@ -8,11 +8,11 @@ require_relative 'http/http'
 # video module
 module Bilibili
   include BiliHttp
-  
+
   # bilibili video interfaces
   class Manga < BilibiliBase
     def check_in
-      user_agent <<~AGENT
+      user_agent = <<~AGENT
         Mozilla/5.0 BiliDroid/6.4.0 (bbcallen@gmail.com) os/android model/M1903F11I mobi_app/android build/6040500 channel/bili innerVer/6040500 osVer/9.0.0 network/2
       AGENT
       cur_header = {}
